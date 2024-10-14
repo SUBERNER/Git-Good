b2c943e757baf742d1faaf26ff2bba7c4666341b
MatthewS2027 <mattlsherman@icloud.com>
2024-10-14 15:11:31 -0500

    MovementControls class
    Class that controls movement input.

---
+++ core/src/main/java/io/github/froggers_revenge/MovementControls.java
@@ -0,0 +1,43 @@
+package io.github.froggers_revenge;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class MovementControls implements KeyListener{
+    private Frogger frogger;
+
+    public MovementControls(Frogger frogger)
+    {
+        this.frogger = frogger;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_UP:
+                frogger.moveUp();
+                break;
+            case KeyEvent.VK_DOWN:
+                frogger.moveDown();
+                break;
+            case KeyEvent.VK_LEFT:
+                frogger.moveLeft();
+                break;
+            case KeyEvent.VK_RIGHT:
+                frogger.moveRight();
+                break;
+            default:
+                break;
+        }
+
+    }
+
+
+
+}
